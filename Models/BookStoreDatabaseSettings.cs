e5a0970c906eb9794d7676039ab13dde592ea020
user <atkahmed9924@gmail.com>
2023-02-06 15:47:47 +0300

    sent codes

---
+++ Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace BookStoreApi.Models
+{//appsettings.json da tanımladıklarımızı buraya birdaha yazıyoruz.
+	public class BookStoreDatabaseSettings
+	{
+		public string ConnectionString { get; set; } = null!;
+		public string DatabaseName { get; set; } = null!;
+		public string BooksCollectionName { get; set; } = null!; 
+
+	}
+}
+
